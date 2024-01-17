df9133c01377a64fb295bb86d43730c6f95e7ceb
cstmatheus <matheus.gom1055@gmail.com>
2024-01-16 20:31:45 -0400

    Atualizado

---
+++ vitaLab_psw/exames/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import TipoExames, SolicitacaoExame, PedidosExames
+
+
+admin.site.register(TipoExames)
+
+admin.site.register(SolicitacaoExame)
+admin.site.register(PedidosExames)
